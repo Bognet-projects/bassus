25e02fb19bc8404fb561058be8117decd517ea96
Bognet-projects <bognar.balazs@bognet.hu>
2020-05-27 09:52:02 +0200

    home page tag system clean up

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,14 +6,16 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Security;
 
 class HomeController extends AbstractController
 {
-    public function index(UserInterface $user){
+    public function index(Security $security){
+        $products = null;
+        if ($security->getUser() != null)
         $products = $this->getDoctrine()
             ->getRepository(Product::class)
-            ->findBy(['available' => true, 'tag'=>$user->getTag()]);
+            ->findBy(['available' => true, 'tag'=>$security->getUser()->getTag()]);
         $all = $this->getDoctrine()
             ->getRepository(Product::class)
             ->findBy(['available' => true]);
